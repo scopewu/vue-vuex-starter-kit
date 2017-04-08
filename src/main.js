3b292c001b8e3e5ad584a5ca7f087143d8e7c92d
scopewu <scopewu@gmail.com>
2017-04-08 16:17:08 +0800

    fix: remove code

--- src/main.js
+++ src/main.js
@@ -2,18 +2,6 @@ import Vue from 'vue';
 
 import router, {routes} from './router'
 
-function a() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve()
-    }, 3000)
-  })
-}
-
-a().then(() => {
-  console.log('success.')
-})
-
 new Vue({
   el: '#root',
   router
