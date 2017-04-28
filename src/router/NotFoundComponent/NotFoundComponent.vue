71d21072c4a4f3fdbb96ab25e1c449e076728170
scopewu <scopewu@gmail.com>
2017-04-29 00:25:19 +0800

    fix: Use router `replace` in 404-not-found

--- src/router/NotFoundComponent/NotFoundComponent.vue
+++ src/router/NotFoundComponent/NotFoundComponent.vue
@@ -3,7 +3,7 @@
     Can't found the path: <b>{{ $route.fullPath }}</b>
     <div>
       Please check the url or Redirect to home page:
-      <router-link to="/">Home</router-link>
+      <router-link to="/" replace>Home</router-link>
     </div>
   </div>
 </template>
